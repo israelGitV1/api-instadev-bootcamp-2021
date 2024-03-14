d68e24ee2af15100e84e5d7445a81c52e3383ac6
israelGitV1 <kasper.war@gmail.com>
2024-03-14 14:53:34 -0300

    create migrations of users and configs

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
+require('./database/index');
 const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+
 app.use(express.json());
 app.use(routes);
 
-const { PORT } = process.env.PORT;
+const { PORT } = process.env;
 app.listen(PORT, () => {
   console.log(`server is on in port ${PORT}`);
 });
